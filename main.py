792dc2582d093ffc5a8f5e308f2cd2c8e414f829
unknown <Softility@SGPL-HL289.sgpltech.com>
2023-08-24 11:26:02 +0530

    I added division feature in main.py file

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ num2 = float(input("Enter the second number: "))
 
 # Perform addition
 sum_result = num1 + num2
+# Perform division
+div_result = num1 + num2
 
 # Display the result
-print("The sum of", num1, "and", num2, "is:", sum_result)
+print("The addition of", num1, "and", num2, "is:", sum_result)
+print("The division of", num1, "and", num2, "is:", div_result)
